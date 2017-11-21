b89486b65ac85a3d846bbf440515fa55f3456a13
chaijq <cz2554@sina.com>
2017-11-21 23:52:22 +0800

    Revert "Merge branch 'master'"
    This reverts commit 62ba25a7b009a4ed448468628ea448ac970f94c5.

--- src/main/java/com/cjq/main/test.java
+++ src/main/java/com/cjq/main/test.java
@@ -4,24 +4,14 @@ import com.cjq.test.GoodsVo;
 
 public class test {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		GoodsVo good=new GoodsVo();
-//			good.name="123";
-			//System.out.println(good.name);
-//			System.out.println("我的第一个分支");
-//			System.out.println(123);
-	try {
-		test();
-	} catch (Exception e) {
-		System.out.println(e.getMessage());
+		GoodsVo good=new GoodsVo();
+			good.name="123";
+			System.out.println(good.name);
+			System.out.println("我的第一个分支");
+			System.out.println(123);
 	}
-				
-		
-	}
-
-	public static void test() throws Exception {
-             if(1==1)throw new Exception("1=2为fasle"); 
-	}
+	
 	
 }
